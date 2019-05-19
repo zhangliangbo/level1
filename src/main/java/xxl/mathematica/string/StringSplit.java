e35d678557e3679044eb45ac46a347447f7e9a4f
zhangliangbo <2472110501@qq.com>
2019-05-19 19:46:57 +0800

    StringSplit

--- src/main/java/xxl/mathematica/string/StringSplit.java
+++ src/main/java/xxl/mathematica/string/StringSplit.java
@@ -3,14 +3,34 @@ package xxl.mathematica.string;
 import xxl.mathematica.ObjectHelper;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * 分割字符串
  */
 public class StringSplit {
-  public static List<String> stringSplit(String str,List<String> splitter) {
-    ObjectHelper.requireNonNull(str);
-    return Arrays.asList(str.split(" "));
+  /**
+   * 按照给定的字符串列表分割字符
+   *
+   * @param str
+   * @param splitter
+   * @return
+   */
+  public static List<String> stringSplit(String str, List<String> splitter) {
+    ObjectHelper.requireNonNull(str, splitter);
+    String split = StringRiffle.stringRiffle(splitter, "|");
+    String[] a = str.split(split);
+    return Arrays.asList(a);
+  }
+
+  /**
+   * 默认空格分隔
+   *
+   * @param str
+   * @return
+   */
+  public static List<String> stringSplit(String str) {
+    return stringSplit(str, Collections.singletonList(" "));
   }
 }
--- src/test/groovy/xxl/mathematica/string/StringSplitTest.groovy
+++ src/test/groovy/xxl/mathematica/string/StringSplitTest.groovy
@@ -3,6 +3,9 @@ package xxl.mathematica.string
 class StringSplitTest extends GroovyTestCase {
 
     void testStringSplit() {
-        println(StringSplit.stringSplit("a b c", null))
+        assertEquals(["a", "b", "c", "d", "e", "f", "g"], StringSplit.stringSplit("a-b:c-d:e-f-g", [":", "-"]))
+        assertEquals(["a-b:c-d:e-f-g"], StringSplit.stringSplit("a-b:c-d:e-f-g"))
     }
+
+
 }
