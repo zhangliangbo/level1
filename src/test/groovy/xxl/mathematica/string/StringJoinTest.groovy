7f220661659f0b8512d757c9d2f3bd30b61c2799
zhangliangbo <2472110501@qq.com>
2019-06-16 14:44:40 +0800

    StringSplit

--- src/test/groovy/xxl/mathematica/string/StringJoinTest.groovy
+++ src/test/groovy/xxl/mathematica/string/StringJoinTest.groovy
@@ -1,11 +1,9 @@
 package xxl.mathematica.string
 
-
-import java.nio.charset.Charset
-
 class StringJoinTest extends GroovyTestCase {
+    void testStringJoin() {
+    }
 
-    void testAbc() {
-        println(Charset.availableCharsets())
+    void testStringJoin1() {
     }
 }
