cd8e8f2d2791a9f7dc030dd0ca20181b03f641f0
zhangliangbo <2472110501@qq.com>
2019-11-01 11:09:55 +0800

    发送邮件

--- src/test/groovy/xxl/mathematica/external/ExternalTest.groovy
+++ src/test/groovy/xxl/mathematica/external/ExternalTest.groovy
@@ -2,10 +2,14 @@ package xxl.mathematica.external
 
 class ExternalTest extends GroovyTestCase {
     void testExec() {
-        println(new String(External.runProcess(new File("C:\\Users\\zhang\\Desktop\\file_server_main_jar"),'mathematica.exe'), 'GBK'))
+        println(new String(External.runProcess(new File("C:\\Users\\zhang\\Desktop\\file_server_main_jar"), 'mathematica.exe'), 'GBK'))
     }
 
     void testRun() {
         println(External.run('git --help'))
+    }
+
+    void testSendMail() {
+        println(External.sendMail("forbidden@aliyun.com", "附件", "<img src=\"https://himg2.huanqiucdn.cn/attachment2010/2019/1101/20191101072831653.jpg\"/>", [new File("C:\\Users\\zhang\\Desktop\\shu.jpg")], "2472110501@qq.com", "smtp.aliyun.com", "forbidden@aliyun.com", "forbidden"))
     }
 }
