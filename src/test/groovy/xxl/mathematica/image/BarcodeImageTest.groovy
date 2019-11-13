fac6aa267134fd54965d08005fb384b0bfde55d1
zhangliangbo <2472110501@qq.com>
2019-11-13 19:56:59 +0800

    生成二维码和识别二维码

--- src/test/groovy/xxl/mathematica/image/BarcodeImageTest.groovy
+++ src/test/groovy/xxl/mathematica/image/BarcodeImageTest.groovy
@@ -1,10 +1,10 @@
 package xxl.mathematica.image
 
-import com.google.zxing.client.j2se.GUIRunner
-
 class BarcodeImageTest extends GroovyTestCase {
     void testBarcodeImage() {
-        println(BarcodeImage.barcodeImage("hello world", 250, "C:\\Users\\zhang\\Desktop\\hello.png"))
+        String file = "C:\\Users\\zhang\\Desktop\\hello.png"
+        println(BarcodeImage.barcodeImage("hello world", 500, file))
+        ShowImage.showImage(file)
     }
 
 }
