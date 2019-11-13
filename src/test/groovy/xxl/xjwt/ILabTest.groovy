6968054cb33c61624ebfec3372daf12cd0588560
zhangliangbo <2472110501@qq.com>
2019-11-13 10:45:37 +0800

    去除敏感信息

--- src/test/groovy/xxl/xjwt/ILabTest.groovy
+++ src/test/groovy/xxl/xjwt/ILabTest.groovy
@@ -6,25 +6,28 @@ import xxl.mathematica.RandomInteger
 import xxl.mathematica.function.Consumer
 
 class ILabTest extends GroovyTestCase {
+    String name = "********";
+    String pwd = "**********"
+
     void testGetUserInfo() {
-        String json = new ILab().getUserInfo("15172384938", "mancivic")
+        String json = new ILab().getUserInfo(name, pwd)
         Map map = new Gson().fromJson(json, Map.class)
         println(map)
     }
 
     void testUploadState() {
-        println(new ILab().uploadState("zhangliangbo"))
+        println(new ILab().uploadState(name))
     }
 
     void testUploadFile() {
         println(new ILab().uploadFile(new File("C:\\Users\\zhang\\Desktop\\comm.pdf")))
     }
-//18674068268
+
     void testUploadResult() {
         Do.loop(new Consumer<Integer>() {
             @Override
             void accept(Integer integer) {
-                println(new ILab().uploadResult("15172384938", "mancivic",
+                println(new ILab().uploadResult(name, pwd,
                         "社会舆情演化模型", 1, RandomInteger.randomInteger(100), System.currentTimeMillis(), RandomInteger.randomInteger(1, 10),
                         new File("C:\\Users\\zhang\\Desktop\\comm.pdf")))
                 Thread.sleep(10000)
