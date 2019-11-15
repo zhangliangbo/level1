34d7ff5d285de2c8d68e39735215daef936361ce
zhangliangbo <2472110501@qq.com>
2019-11-15 16:26:52 +0800

    采用三段版本号

--- src/test/groovy/xxl/wxpay/WXPayTest.groovy
+++ src/test/groovy/xxl/wxpay/WXPayTest.groovy
@@ -11,11 +11,13 @@ class WXPayTest extends GroovyTestCase {
     String aliAppId
     boolean isSandbox = false
     String sandboxKey = "0920788a26a4db959a11015e4f2b7beb"
+    String wxNotifyPrefix
 
     @Override
     protected void setUp() throws Exception {
         Map<String, Object> hello = Import.importJson("D:\\xxlun\\xxlun\\微信平台\\微信商户平台\\hello.json")
         mchId = hello.get("mch_id")
+        wxNotifyPrefix = hello.get("notify")
         Pay.registerWx(hello.get("mch_id") as String, hello.get("cert") as String, hello.get("appid") as String, hello.get("key") as String)
         Map<String, Object> ali = Import.importJson("D:\\xxlun\\xxlun\\阿里平台\\ali.json")
         aliAppId = ali.get("appId") as String
@@ -30,7 +32,7 @@ class WXPayTest extends GroovyTestCase {
     void testWxOrder() {
         String outTradeNo = ID.snowflake(1)
         println(outTradeNo)
-        Map<String, String> code = Pay.wxBarcode(mchId, outTradeNo, 1, "A座-1509", "这是一个商品详情iPhone", "6688", "121.60.117.78", "http://www.weixin.qq.com/wxpay/pay.php")
+        Map<String, String> code = Pay.wxBarcode(mchId, outTradeNo, 1, "A座-1509", "这是一个商品详情iPhone", "6688", "121.60.117.78", wxNotifyPrefix + "/pay/unifiedorder")
         println(code)
         ShowImage.showImage(BarcodeImage.barcodeImage(code.get("code_url")))
     }
