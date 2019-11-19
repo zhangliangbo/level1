04ee73a6dcd7ac714f3d356226ac30166192d5bc
zhangliangbo <2472110501@qq.com>
2019-11-19 08:57:06 +0800

    日期字符串

---
+++ src/main/java/xxl/mathematica/time/DateString.java
@@ -0,0 +1,40 @@
+package xxl.mathematica.time;
+
+import org.apache.commons.lang3.time.DateFormatUtils;
+
+import java.util.Date;
+
+/**
+ * 日期字符串
+ */
+public class DateString {
+  /**
+   * 日期字符串
+   *
+   * @param date
+   * @param format
+   * @return
+   */
+  public static String dateString(Date date, String format) {
+    return DateFormatUtils.format(date, format);
+  }
+
+  /**
+   * 默认yyyy-MM-dd HH:mm:ss
+   *
+   * @param date
+   * @return
+   */
+  public static String dateString(Date date) {
+    return dateString(date, "yyyy-MM-dd HH:mm:ss");
+  }
+
+  /**
+   * 当前时间的字符串
+   *
+   * @return
+   */
+  public static String dateString() {
+    return dateString(new Date(System.currentTimeMillis()));
+  }
+}
