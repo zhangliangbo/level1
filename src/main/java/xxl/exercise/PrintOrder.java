f560a3b64f61733afa2a7c62b92d81fa371fd9c5
zhangliangbo <2472110501@qq.com>
2020-02-11 11:16:43 +0800

    报数

---
+++ src/main/java/xxl/exercise/PrintOrder.java
@@ -0,0 +1,46 @@
+package xxl.exercise;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+/**
+ * 打印顺序
+ */
+public class PrintOrder {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    while (sc.hasNextLine()) {
+      String line = sc.nextLine();
+      String[] weights = line.split(" +");
+      Deque<Task> deque = new LinkedList<>();
+      for (int i = 0; i < weights.length; i++) {
+        deque.offerFirst(new Task(i, Integer.parseInt(weights[i])));
+      }
+      while (!deque.isEmpty()) {
+        Task current = deque.pollFirst();
+        boolean moved = false;
+        for (Task t : deque) {
+          if (current.weight < t.weight) {
+            deque.addLast(current);
+            moved = true;
+            break;
+          }
+        }
+        if (!moved) {
+          System.out.println(current.index + " ");
+        }
+      }
+    }
+  }
+
+  static class Task {
+    int index;
+    int weight;
+
+    public Task(int index, int weight) {
+      this.index = index;
+      this.weight = weight;
+    }
+  }
+}
