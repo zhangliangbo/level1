638d7857572d1dad1833b503b5e7e3b6422c5ab9
zhangliangbo <2472110501@qq.com>
2020-02-16 20:38:53 +0800

    kafka

--- src/main/java/xxl/kafka/ConsumerDemo.java
+++ src/main/java/xxl/kafka/ConsumerDemo.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class ConsumerDemo {
   public static void main(String[] args) {
     KfkConsumer consumer = new KfkConsumer(new String[]{"localhost:9094"}, "test");
-    consumer.subscribe("test");
+    consumer.assign("test", 0);
     consumer.seekToBeginning("test", 0);
     while (true) {
       List<Record> records = consumer.poll(1000);
