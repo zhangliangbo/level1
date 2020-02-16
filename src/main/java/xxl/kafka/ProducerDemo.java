25a238b0f5e4a9bf4081d9dd3f68a30f4df7313c
zhangliangbo <2472110501@qq.com>
2020-02-16 20:58:49 +0800

    kafka

--- src/main/java/xxl/kafka/ProducerDemo.java
+++ src/main/java/xxl/kafka/ProducerDemo.java
@@ -7,7 +7,7 @@ public class ProducerDemo {
   public static void main(String[] args) {
     KfkProducer producer = new KfkProducer(new String[]{"localhost:9094"}, "all");
     for (int i = 0; i < 100; i++) {
-      long offset = producer.send("test", "hello" + i);
+      long offset = producer.send("test", "key" + i, "hello" + i);
       System.err.println("offset is " + offset);
     }
     producer.close();
