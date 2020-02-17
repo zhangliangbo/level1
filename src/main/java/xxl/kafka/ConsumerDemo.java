d0863387e677362251a1cf4b7c896b823e64184f
zhangliangbo <2472110501@qq.com>
2020-02-17 18:26:32 +0800

    使用command-cli设置参数

--- src/main/java/xxl/kafka/ConsumerDemo.java
+++ src/main/java/xxl/kafka/ConsumerDemo.java
@@ -1,19 +1,36 @@
 package xxl.kafka;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import java.util.List;
 
 public class ConsumerDemo {
   public static void main(String[] args) {
-    KfkConsumer consumer = new KfkConsumer(new String[]{"localhost:9094", "localhost:9095", "localhost:9096"}, "group1");
-    consumer.subscribe("xxl");
-    while (true) {
-      List<Record> records = consumer.poll(1000);
-      if (records != null) {
-        for (Record record : records) {
-          System.err.println(record);
-          consumer.commitSync();
+    Options options = new Options().addOption("group", true, "指定消费组");
+    CommandLine cli;
+    try {
+      cli = new DefaultParser().parse(options, args);
+    } catch (ParseException e) {
+      System.out.println(options);
+      return;
+    }
+    if (cli.hasOption("group")) {
+      KfkConsumer consumer = new KfkConsumer(new String[]{"localhost:9094", "localhost:9095", "localhost:9096"}, cli.getOptionValue("group"));
+      consumer.subscribe("xxl");
+      while (true) {
+        List<Record> records = consumer.poll(1000);
+        if (records != null) {
+          for (Record record : records) {
+            System.err.println(record);
+            consumer.commitSync();
+          }
         }
       }
+    } else {
+      System.out.println(options);
     }
   }
 }
