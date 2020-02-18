2ff1b6fce19696b18a19db73f7b1eb35d099ae32
zhangliangbo <2472110501@qq.com>
2020-02-18 23:16:47 +0800

    kafka-streams demo

---
+++ src/main/java/xxl/flink/FKSerialization.java
@@ -0,0 +1,15 @@
+package xxl.flink;
+
+import org.apache.flink.streaming.connectors.kafka.KafkaSerializationSchema;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import xxl.kafka.Record;
+
+import javax.annotation.Nullable;
+
+public class FKSerialization implements KafkaSerializationSchema<Record> {
+
+  @Override
+  public ProducerRecord<byte[], byte[]> serialize(Record element, @Nullable Long timestamp) {
+    return null;
+  }
+}
