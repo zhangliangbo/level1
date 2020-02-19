e7c0667d602110de9af675fb45d470b8320900cb
zhangliangbo <2472110501@qq.com>
2020-02-19 21:59:26 +0800

    flink kafka

--- src/main/java/xxl/flink/KafkaDemo.java
+++ src/main/java/xxl/flink/KafkaDemo.java
@@ -1,5 +1,6 @@
 package xxl.flink;
 
+import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import xxl.kafka.Record;
@@ -7,14 +8,19 @@ import xxl.kafka.Record;
 public class KafkaDemo {
   public static void main(String[] args) throws Exception {
     final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-    FKConsumer consumer = new FKConsumer(new String[]{"localhost:9094", "localhost:9095", "localhost:9096"}, "kou", "flink", true);
-    FKProducer producer = new FKProducer(new String[]{"localhost:9094", "localhost:9095", "localhost:9096"}, -1, "xxl");
+    FKConsumer consumer = new FKConsumer(new String[]{"kafka1:9092", "kafka2:9092", "kafka3:9092"}, "kou", "flink", true);
+    FKProducer producer = new FKProducer(new String[]{"kafka1:9092", "kafka2:9092", "kafka3:9092"}, -1, "xxl");
     env.addSource(consumer)
+        .filter(new FilterFunction<Record>() {
+          @Override
+          public boolean filter(Record value) throws Exception {
+            return value.value().contains("0");
+          }
+        })
         .map(new MapFunction<Record, Record>() {
           @Override
           public Record map(Record value) throws Exception {
-            System.err.println("get a " + value.key() + ":" + value.value());
-            return new Record("xxl", value.key() + " flink", value.value() + " flink");
+            return new Record("xxl", value.key(), value.value());
           }
         })
         .addSink(producer);
