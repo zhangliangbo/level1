5eed14553c43c9ea12551cc2ecfdfcd3949f5cf5
zhangliangbo <2472110501@qq.com>
2020-02-19 18:26:17 +0800

    flink kafka

--- src/main/java/xxl/flink/KafkaDemo.java
+++ src/main/java/xxl/flink/KafkaDemo.java
@@ -1,5 +1,25 @@
 package xxl.flink;
 
-public class KafkaDemo {
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import xxl.kafka.Record;
 
+public class KafkaDemo {
+  public static void main(String[] args) throws Exception {
+    final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+    env.enableCheckpointing(5000);
+    FKConsumer consumer = new FKConsumer(new String[]{"localhost:9094", "localhost:9095", "localhost:9096"}, "flink", "flink", false);
+    consumer.setStartFromEarliest();
+    FKProducer producer = new FKProducer(new String[]{"localhost:9094", "localhost:9095", "localhost:9096"}, -1, "xxl");
+    producer.setWriteTimestampToKafka(true);
+    env.addSource(consumer)
+        .map(new MapFunction<Record, Record>() {
+          @Override
+          public Record map(Record value) throws Exception {
+            return null;
+          }
+        })
+        .addSink(producer);
+    env.execute("KafkaDemo");
+  }
 }
