e89d36084786e77c52031db710eebaba68512f77
zhangliangbo <2472110501@qq.com>
2020-02-19 21:12:48 +0800

    flink kafka

--- src/main/java/xxl/flink/FKProducer.java
+++ src/main/java/xxl/flink/FKProducer.java
@@ -4,6 +4,8 @@ import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 import xxl.kafka.KfkProducer;
 import xxl.kafka.Record;
 
+import java.util.Properties;
+
 /**
  * 生产者
  */
@@ -11,5 +13,12 @@ public class FKProducer extends FlinkKafkaProducer<Record> {
   public FKProducer(String[] servers, int ack, String topic) {
     super(topic, new FKSerialization(), KfkProducer.props(servers, ack),
         ack == 0 ? Semantic.NONE : (ack == -1 ? Semantic.EXACTLY_ONCE : Semantic.AT_LEAST_ONCE));
+  }
+
+  private static Properties props(String[] servers, int ack) {
+    Properties props = KfkProducer.props(servers, ack);
+    props.put("key.deserializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
+    props.put("value.deserializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
+    return props;
   }
 }
--- src/main/java/xxl/flink/KafkaDemo.java
+++ src/main/java/xxl/flink/KafkaDemo.java
@@ -7,15 +7,13 @@ import xxl.kafka.Record;
 public class KafkaDemo {
   public static void main(String[] args) throws Exception {
     final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-    env.enableCheckpointing(5000);
     FKConsumer consumer = new FKConsumer(new String[]{"localhost:9094", "localhost:9095", "localhost:9096"}, "kou", "flink", true);
-    consumer.setStartFromEarliest();
     FKProducer producer = new FKProducer(new String[]{"localhost:9094", "localhost:9095", "localhost:9096"}, -1, "xxl");
-    producer.setWriteTimestampToKafka(true);
     env.addSource(consumer)
         .map(new MapFunction<Record, Record>() {
           @Override
           public Record map(Record value) throws Exception {
+            System.err.println("get a " + value.key() + ":" + value.value());
             return new Record("xxl", value.key() + " flink", value.value() + " flink");
           }
         })
