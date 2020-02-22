1635086ef4e19402c34ec6a291d01055719a3ad8
zhangliangbo <2472110501@qq.com>
2020-02-22 21:56:12 +0800

    paho mqtt

--- src/main/java/xxl/mqtt/PahoOptions.java
+++ src/main/java/xxl/mqtt/PahoOptions.java
@@ -3,12 +3,13 @@ package xxl.mqtt;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 
 public class PahoOptions extends MqttConnectOptions {
-  public PahoOptions(String username, String password, boolean autoReconnect, boolean cleanSession) {
+  public PahoOptions(String username, String password, boolean autoReconnect, boolean cleanSession, PahoMessage will) {
     setUserName(username);
     if (password != null) {
       setPassword(password.toCharArray());
     }
     setAutomaticReconnect(autoReconnect);
     setCleanSession(cleanSession);
+    setWill(will.topic(), will.body(), will.qos(), will.retain());
   }
 }
