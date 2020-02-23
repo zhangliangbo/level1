9517d8aafd44dcb9b4b1c19ad4cee21c9e6bf3e1
zhangliangbo <2472110501@qq.com>
2020-02-23 22:02:08 +0800

    flink rabbitmq

--- src/main/java/xxl/flink/FRConsumer.java
+++ src/main/java/xxl/flink/FRConsumer.java
@@ -3,11 +3,27 @@ package xxl.flink;
 import org.apache.flink.streaming.connectors.rabbitmq.RMQSource;
 import xxl.rabbitmq.Record;
 
+import java.io.IOException;
+
 /**
  * rabbitmq消费者
  */
 public class FRConsumer extends RMQSource<Record> {
-  public FRConsumer(String host, Integer port, String username, String password, String vHost, String queueName) {
+
+  private String exchange;
+  private String routingKey;
+
+  public FRConsumer(String host, Integer port, String username, String password, String vHost, String queueName, String exchange, String routingKey) {
     super(FRUtil.config(host, port, username, password, vHost), queueName, new FRDeserialization());
+    this.exchange = exchange;
+    this.routingKey = routingKey;
+  }
+
+  @Override
+  protected void setupQueue() throws IOException {
+    super.setupQueue();
+    if (exchange != null) {
+      channel.queueBind(queueName, exchange, routingKey);
+    }
   }
 }
