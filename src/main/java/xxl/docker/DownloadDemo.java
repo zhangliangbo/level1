d78531f6f5cbbc721e21fa04289ae533c83a1558
zhangliangbo <2472110501@qq.com>
2020-02-23 18:10:17 +0800

    docker download image

--- src/main/java/xxl/docker/DownloadDemo.java
+++ src/main/java/xxl/docker/DownloadDemo.java
@@ -77,7 +77,7 @@ public class DownloadDemo {
       try {
         Thread.sleep(500);
       } catch (InterruptedException e) {
-        e.printStackTrace();
+        System.err.println(e.getMessage());
       }
     }
   }
