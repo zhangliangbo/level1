e00d2880c7367499b7afb819bb75cb476321cbb7
zhangliangbo <2472110501@qq.com>
2020-02-23 22:19:23 +0800

    flink rabbitmq

--- src/main/java/xxl/flink/FRConsumer.java
+++ src/main/java/xxl/flink/FRConsumer.java
@@ -14,11 +14,38 @@ public class FRConsumer extends RMQSource<Record> {
   private String type;
   private String routingKey;
 
+  /**
+   * 绑定交换器
+   *
+   * @param host
+   * @param port
+   * @param username
+   * @param password
+   * @param vHost
+   * @param queueName
+   * @param exchange
+   * @param type
+   * @param routingKey
+   */
   public FRConsumer(String host, Integer port, String username, String password, String vHost, String queueName, String exchange, String type, String routingKey) {
     super(FRUtil.config(host, port, username, password, vHost), queueName, new FRDeserialization());
     this.exchange = exchange;
     this.type = type;
     this.routingKey = routingKey;
+  }
+
+  /**
+   * 不绑定交换器
+   *
+   * @param host
+   * @param port
+   * @param username
+   * @param password
+   * @param vHost
+   * @param queueName
+   */
+  public FRConsumer(String host, Integer port, String username, String password, String vHost, String queueName) {
+    this(host, port, username, password, vHost, queueName, null, null, null);
   }
 
   @Override
