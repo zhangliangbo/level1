2072bc37ddfabbeecb7087c5214d47497b67be12
zhangliangbo <2472110501@qq.com>
2020-05-14 10:43:51 +0800

    使用java8接口和vavr实现

--- src/main/java/xxl/mathematica/list/Take.java
+++ src/main/java/xxl/mathematica/list/Take.java
@@ -7,6 +7,7 @@ import java.util.List;
  */
 
 public class Take {
+
     /**
      * 给出list的前n个或者后n个元素
      *
@@ -24,7 +25,7 @@ public class Take {
     }
 
     /**
-     * 给出 list 中从 m（包括） 到 n（不包括） 的元素
+     * 步长为1
      *
      * @param list
      * @param m
@@ -33,7 +34,20 @@ public class Take {
      * @return
      */
     public static <T> List<T> take(List<T> list, int m, int n) {
-        return io.vavr.collection.List.range(m < 0 ? m + list.size() : m, n < 0 ? n + list.size() : n)
+        return take(list, m, n, 1);
+    }
+
+    /**
+     * 给出 list 中从 m（包括） 到 n（不包括） 的元素
+     *
+     * @param list
+     * @param m
+     * @param n
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> take(List<T> list, int m, int n, int step) {
+        return io.vavr.collection.List.rangeBy(m < 0 ? m + list.size() : m, n < 0 ? n + list.size() : n, step)
                 .map(list::get)
                 .asJava();
     }
