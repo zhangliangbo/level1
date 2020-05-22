1d9d6ccb3e30275414c37b763aa6c3ff811182b9
zhangliangbo <2472110501@qq.com>
2020-05-22 08:57:09 +0800

    Power Programming：Resort

--- src/main/java/xxl/Main.java
+++ src/main/java/xxl/Main.java
@@ -1,22 +1,30 @@
 package xxl;
 
-import org.apache.commons.lang3.time.StopWatch;
+import io.vavr.collection.List;
+
+import java.util.function.BiFunction;
 
 public class Main {
 
 
     public static void main(String[] args) throws InterruptedException {
-        StopWatch watch = new StopWatch();
-        watch.start();
-        Thread.sleep(1000);
-        watch.split();
-        System.err.println(watch.formatSplitTime());
-        Thread.sleep(1000);
-        watch.split();
-        System.err.println(watch.formatSplitTime());
-        Thread.sleep(1000);
-        watch.split();
-        System.err.println(watch.formatSplitTime());
-        watch.stop();
+        System.err.println(
+                List.rangeBy(0, 10, 1)
+                        .reduce(new BiFunction<Integer, Integer, Integer>() {
+                            @Override
+                            public Integer apply(Integer integer, Integer integer2) {
+                                return integer + integer2;
+                            }
+                        })
+        );
+        System.err.println(
+                List.rangeBy(0, 10, 1)
+                        .fold(0, new BiFunction<Integer, Integer, Integer>() {
+                            @Override
+                            public Integer apply(Integer integer, Integer integer2) {
+                                return integer + integer2;
+                            }
+                        })
+        );
     }
 }
