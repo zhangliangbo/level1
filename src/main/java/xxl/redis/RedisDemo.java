155cc3c55d1833b174e28f9aed1d4e99ee978ba1
zhangliangbo <2472110501@qq.com>
2020-06-15 20:47:54 +0800

    redis

--- src/main/java/xxl/redis/RedisDemo.java
+++ src/main/java/xxl/redis/RedisDemo.java
@@ -2,6 +2,8 @@ package xxl.redis;
 
 import redis.clients.jedis.Jedis;
 
+import java.util.function.Consumer;
+
 public class RedisDemo {
   public static void main(String[] args) {
     Jedis jedis = new Jedis("redis", 6379);
@@ -11,5 +13,16 @@ public class RedisDemo {
     } else {
       System.err.println("not connected.");
     }
+    long start = System.currentTimeMillis();
+    System.err.println("flush all: " + jedis.flushAll());
+    io.vavr.collection.List.range(0, 10000000)
+        .forEach(new Consumer<Integer>() {
+          @Override
+          public void accept(Integer integer) {
+            System.err.println(jedis.set(String.valueOf(integer), String.valueOf(integer)));
+          }
+        });
+    System.err.println("bgsave: " + jedis.bgsave());
+    System.err.println("time: " + (System.currentTimeMillis() - start));
   }
 }
