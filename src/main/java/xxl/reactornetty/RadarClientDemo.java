2dd992d59ea1bb29a30e99ce511a4e3a5ba70ca2
zhangliangbo <2472110501@qq.com>
2020-07-04 11:35:56 +0800

    LengthFieldBasedFrameDecoder粘包和拆包解码器 ^表示异或，不是表示乘方

---
+++ src/main/java/xxl/reactornetty/RadarClientDemo.java
@@ -0,0 +1,94 @@
+package xxl.reactornetty;
+
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.nio.ByteOrder;
+import java.util.Arrays;
+
+public class RadarClientDemo {
+  public static void main(String[] args) throws InterruptedException, FileNotFoundException {
+    final String host = "localhost";
+    final int port = 8080;
+    final NioEventLoopGroup group = new NioEventLoopGroup();
+    FileOutputStream raw = new FileOutputStream(System.getProperty("user.dir") + File.separator + "raw.txt");
+    FileOutputStream split = new FileOutputStream(System.getProperty("user.dir") + File.separator + "split.txt");
+    System.err.println("start connected.");
+    new Bootstrap().channel(NioSocketChannel.class)
+        .group(group)
+        .handler(new ChannelInitializer<Channel>() {
+          @Override
+          protected void initChannel(Channel ch) throws Exception {
+            ch.pipeline().addLast(new LengthFieldBasedFrameDecoder(ByteOrder.BIG_ENDIAN, Short.MAX_VALUE, 1, 2, -3, 0, true));
+            ch.pipeline().addLast(new SimpleChannelInboundHandler<ByteBuf>() {
+              @Override
+              public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                super.channelActive(ctx);
+                System.err.println("active");
+              }
+
+              @Override
+              public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+                super.channelInactive(ctx);
+                System.err.println("inactive");
+              }
+
+              @Override
+              public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+                super.channelRegistered(ctx);
+                System.err.println("registered");
+              }
+
+              @Override
+              public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+                super.channelUnregistered(ctx);
+                System.err.println("unregistered");
+              }
+
+              @Override
+              public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+                super.exceptionCaught(ctx, cause);
+                raw.close();
+                split.close();
+              }
+
+              @Override
+              protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+                byte[] bytes = ByteBufUtil.getBytes(msg);
+                String string = new String(bytes);
+                System.err.println(Arrays.toString(bytes));
+                System.err.println(string);
+                raw.write(string.getBytes());
+                split.write((string + "\n\n").getBytes());
+              }
+            });
+          }
+        })
+        .connect(host, port)
+        .sync()
+        .addListener(new ChannelFutureListener() {
+          @Override
+          public void operationComplete(ChannelFuture future) throws Exception {
+            System.err.println("connected.");
+          }
+        })
+        .channel()
+        .closeFuture()
+        .addListener((ChannelFutureListener) future -> {
+          group.shutdownGracefully();
+          raw.close();
+          split.close();
+        })
+        .sync();
+    System.err.println("end connected.");
+  }
+}
